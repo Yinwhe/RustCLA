358d09878b399b9ee82c0da394dbbdabc44c558f
Yinwhe <wyffeiwhe@gmail.com>
2023-09-08 15:51:32 +0800

    update: get ffi funcs

--- ABIChecker/src/analysis/analysis_types/ir_info.rs
+++ ABIChecker/src/analysis/analysis_types/ir_info.rs
@@ -7,8 +7,10 @@ use std::io::Write;
 pub struct IRInfo<'a> {
     r_modules: Vec<Module<'a>>,
     c_modules: Vec<Module<'a>>,
-    r_functions: HashMap<String, FunctionValue<'a>>,
-    c_functions: HashMap<String, FunctionValue<'a>>,
+    rdef_functions: HashMap<String, FunctionValue<'a>>,
+    rdec_functions: HashMap<String, FunctionValue<'a>>,
+    cdef_functions: HashMap<String, FunctionValue<'a>>,
+    cdec_functions: HashMap<String, FunctionValue<'a>>,
     ffi_functions: Vec<String>,
 }
 
@@ -17,15 +19,17 @@ impl IRInfo<'_> {
         IRInfo {
             r_modules: r,
             c_modules: c,
-            r_functions: HashMap::new(),
-            c_functions: HashMap::new(),
+            rdef_functions: HashMap::new(),
+            rdec_functions: HashMap::new(),
+            cdef_functions: HashMap::new(),
+            cdec_functions: HashMap::new(),
             ffi_functions: Vec::new(),
         }
     }
 
     pub fn get_ffi(&mut self) -> Result<(), String> {
         self.get_ffi_funcs()?;
-        
+
         Ok(())
     }
 
@@ -34,36 +38,48 @@ impl IRInfo<'_> {
     }
 
     pub fn c_func(&self, name: &str) -> Option<&FunctionValue> {
-        self.c_functions.get(name)
+        self.cdef_functions
+            .get(name)
+            .or(self.cdec_functions.get(name))
     }
 
     pub fn r_func(&self, name: &str) -> Option<&FunctionValue> {
-        self.r_functions.get(name)
+        self.rdef_functions
+            .get(name)
+            .or(self.rdec_functions.get(name))
     }
 
     /// Locate FFI Functions.
     fn get_ffi_funcs(&mut self) -> Result<(), String> {
         self.prepare_functions()?;
-        let rf_names = self.r_functions.keys().collect::<HashSet<&String>>();
-        let cf_names = self.c_functions.keys().collect::<HashSet<&String>>();
 
-
-        // println!("{:?}", rf_names);
-        // println!("{:?}", cf_names);
-
-        self.ffi_functions = rf_names
-            .intersection(&cf_names)
+        let r_use_c = self
+            .rdec_functions
+            .keys()
+            .collect::<HashSet<&String>>()
+            .intersection(&self.cdef_functions.keys().collect::<HashSet<&String>>())
             .map(|&name| name.to_owned())
             .collect::<Vec<String>>();
 
-        // functions backup and mannually check
-        let mut rf_debug = File::create("rf_debug.txt").unwrap();
-        let s = serde_json::to_string(&rf_names).unwrap();
-        writeln!(rf_debug, "{}", s).unwrap();
+        let c_use_r = self
+            .cdec_functions
+            .keys()
+            .collect::<HashSet<&String>>()
+            .intersection(&self.rdef_functions.keys().collect::<HashSet<&String>>())
+            .map(|&name| name.to_owned())
+            .collect::<Vec<String>>();
 
-        let mut cf_debug = File::create("cf_debug.txt").unwrap();
-        let s = serde_json::to_string(&cf_names).unwrap();
-        writeln!(cf_debug, "{}", s).unwrap();
+        let ffi_funcs = Vec::from_iter(r_use_c.into_iter().chain(c_use_r.into_iter()));
+        self.ffi_functions = ffi_funcs;
+
+        // functions backup and mannually check
+        // let mut rf_debug = File::create("rf_debug.txt").unwrap();
+        // let s = serde_json::to_string(&rf_names).unwrap();
+        // writeln!(rf_debug, "{}", s).unwrap();
+
+        // let mut cf_debug = File::create("cf_debug.txt").unwrap();
+        // let s = serde_json::to_string(&cf_names).unwrap();
+        // writeln!(cf_debug, "{}", s).unwrap();
 
         Ok(())
     }
@@ -85,7 +101,13 @@ impl IRInfo<'_> {
         for rm in &self.r_modules {
             for f in rm.get_functions() {
                 let name = strip(f.get_name().to_str().map_err(|e| e.to_string())?).to_owned();
-                if let Some(f) = self.r_functions.insert(name, f) {
+                let ret = if f.count_basic_blocks() == 0 {
+                    self.rdec_functions.insert(name, f)
+                } else {
+                    self.rdef_functions.insert(name, f)
+                };
+
+                if let Some(f) = ret {
                     return Err(format!("Duplicate function name: {:?}", f.get_name()));
                 }
             }
@@ -94,7 +116,13 @@ impl IRInfo<'_> {
         for cm in &self.c_modules {
             for f in cm.get_functions() {
                 let name = strip(f.get_name().to_str().map_err(|e| e.to_string())?).to_owned();
-                if let Some(f) = self.c_functions.insert(name, f) {
+                let ret = if f.count_basic_blocks() == 0 {
+                    self.cdec_functions.insert(name, f)
+                } else {
+                    self.cdef_functions.insert(name, f)
+                };
+
+                if let Some(f) = ret {
                     return Err(format!("Duplicate function name: {:?}", f.get_name()));
                 }
             }
